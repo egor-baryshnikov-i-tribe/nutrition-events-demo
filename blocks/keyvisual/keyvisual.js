5a8a62ea57b1c203d83c302f577daa3eb4eabbac
Egor Baryshnikov <egor.baryshnikov@i-tribe.de>
2025-05-28 15:43:06 +0500

    [DEV] Keyvisual fixes

--- blocks/keyvisual/keyvisual.js
+++ blocks/keyvisual/keyvisual.js
@@ -3,8 +3,7 @@ export default async function decorate(block) {
   const text = block.querySelectorAll('div div')[2] ?? null;
   const position = block.querySelectorAll('div div')[3] ?? null;
 
-  const keyvisual = document.createElement('div')
-  keyvisual.classList.add('keyvisual');
+
   image.classList.add('keyvisual-image');
   text.classList.add('text');
   text.classList.add(...position.querySelector('p').innerHTML.split(' '));
@@ -14,4 +13,5 @@ export default async function decorate(block) {
   content.querySelector('a').classList.add('button')
   block.innerHTML = '';
   block.append(image, content);
+  block.outerHTML = block.innerHTML;
 }
